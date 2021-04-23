b9a43c4a5e963ed4078426f68aadfbfbd8553f6a
Josiah <josiah.pilon@yahoo.ca>
2021-04-23 16:58:35 -0700

    Components for loading screen and the panels
    Until a panel is clicked on, all 4 panels will show in a grid

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,78 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+import Loading from 'components/Loading';
+import Panel from 'components/Panel';
 
-    return <main className={dashboardClasses} />;
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
+// Add a new function to the Dashboard component class that can take an id and set the state of focused to the value of id.
+// Pass the function to the Panel component so that it can call it with the id of the Panel that we click.
+// Call the function with the id in an onClick event handler on the root element of the Panel 
+// component.
+
+class Dashboard extends Component {
+
+  state = {
+    loading: false,
+    focused: null
+  }
+
+  selectPanel(id) {
+    this.setState({
+     focused: id
+    });
+   }
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = data
+   .filter(
+    panel => this.state.focused === null || this.state.focused === panel.id
+   )
+   .map(panel => (
+    <Panel
+     key={panel.id}
+     id={panel.id}
+     label={panel.label}
+     value={panel.value}
+     onSelect={this.selectPanel}
+    />
+   ));
+
+    return (
+    <main className={dashboardClasses} >
+    {panels}
+    </main>
+    );
   }
 }
 
